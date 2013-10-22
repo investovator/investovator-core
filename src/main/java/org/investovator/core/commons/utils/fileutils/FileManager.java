910a6de9a8e2dcc1624671ff25b2211b97f1fc6b
Rajith Siriwardana <rajithsiriwardana@gmail.com>
2013-10-23 00:22:20 +0530

    FileManager added, MySQL connector utility added and resource file modified and sql schema added

--- src/main/java/org/investovator/core/commons/utils/fileutils/FileManager.java
+++ src/main/java/org/investovator/core/commons/utils/fileutils/FileManager.java
@@ -1,4 +1,26 @@
+/*
+ * investovator, Stock Market Gaming framework
+ * Copyright (C) 2013  investovator
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.investovator.core.commons.utils.fileutils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileManager {
 
@@ -24,3 +46,23 @@ public class FileManager {
         reader.close();
         return stringBuilder.toString();
     }
+
+    public static String readFileOmitComments(String filePath, String commentPattern) throws IOException {
+        BufferedReader reader;
+        StringBuilder stringBuilder;
+        String line;
+        String ls;
+        reader = new BufferedReader(new FileReader(filePath));
+        stringBuilder = new StringBuilder();
+        ls = System.getProperty("line.separator");
+
+        while ((line = reader.readLine()) != null) {
+            if(!line.startsWith(commentPattern)){
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+        }
+        reader.close();
+        return stringBuilder.toString();
+    }
+}
---
+++ src/main/java/org/investovator/core/data/rssexplorer/utils/ConnectorUtils.java
@@ -0,0 +1,54 @@
+/*
+ * investovator, Stock Market Gaming framework
+ * Copyright (C) 2013  investovator
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.investovator.core.data.rssexplorer.utils;
+
+import org.investovator.core.commons.utils.fileutils.FileManager;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author rajith
+ * @version $Revision$
+ */
+public class ConnectorUtils {
+
+    private static final String COMMENT_PATTERN = "--";
+    private static final String DELIMITER = ";";
+
+    public static void executeSQLFile(String username, String password, String url, String relativeFilePath)
+            throws IOException, SQLException {
+        String sqlStatement = FileManager.readFileOmitComments(relativeFilePath, COMMENT_PATTERN);
+        Connection conn = DriverManager.getConnection(url, username, password);
+
+        Statement statement = conn.createStatement();
+        String[] formattedSQLStrings = sqlStatement.split(DELIMITER);
+
+        for(String formattedSQLString: formattedSQLStrings){
+            if(!(formattedSQLString.trim().equals(""))){
+                statement.executeUpdate(formattedSQLString + DELIMITER);
+            }
+        }
+        statement.close();
+        conn.close();
+    }
+}
