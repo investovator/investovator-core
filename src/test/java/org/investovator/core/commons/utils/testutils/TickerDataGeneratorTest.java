9cf328bac765fe5593d1aafa2127f0222abff557
Amila Surendra <amilasurendra@gmail.com>
2013-11-14 12:30:00 +0530

    Added test CSV ang class for testing Data Generator.

---
+++ src/test/java/org/investovator/core/commons/utils/testutils/TickerDataGeneratorTest.java
@@ -0,0 +1,14 @@
+package org.investovator.core.commons.utils.testutils;
+
+import org.junit.Test;
+
+/**
+ * @author Amila Surendra
+ * @version $Revision
+ */
+public class TickerDataGeneratorTest {
+    @Test
+    public void testCreateCSV() throws Exception {
+
+    }
+}
